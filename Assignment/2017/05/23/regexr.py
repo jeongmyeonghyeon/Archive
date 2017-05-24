6ec2c7af91f5d891ac19c7a2efeadc656969d320
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-05-25 01:16:40 +0900

    add 5월24일 과제

--- Assignment/2017/05/23/regexr.py
+++ Assignment/2017/05/23/regexr.py
@@ -3,16 +3,41 @@ import re
 story = '''Born to the prestigious Crownguards, the paragon family of Demacian service, Luxanna was destined for greatness. She grew up as the family's only daughter, and she immediately took to the advanced education and lavish parties required of families as high profile as the Crownguards. As Lux matured, it became clear that she was extraordinarily gifted. She could play tricks that made people believe they had seen things that did not actually exist. She could also hide in plain sight. Somehow, she was able to reverse engineer arcane magical spells after seeing them cast only once. She was hailed as a prodigy, drawing the affections of the Demacian government, military, and citizens alike.
 As one of the youngest women to be tested by the College of Magic, she was discovered to possess a unique command over the powers of light. The young Lux viewed this as a great gift, something for her to embrace and use in the name of good. Realizing her unique skills, the Demacian military recruited and trained her in covert operations. She quickly became renowned for her daring achievements; the most dangerous of which found her deep in the chambers of the Noxian High Command. She extracted valuable inside information about the Noxus-Ionian conflict, earning her great favor with Demacians and Ionians alike. However, reconnaissance and surveillance was not for her. A light of her people, Lux's true calling was the League of Legends, where she could follow in her brother's footsteps and unleash her gifts as an inspiration for all of Demacia.'''
 
+# 1번
 # {m}패턴지정자를 사용해서 a로 시작하는 4글자 단어를 전부 찾는다.
 print(re.findall('\sa\w{3}\s', story))
 
+# 해답
+# 위처럼 하니까 공백문자가 포함되는 문제가 있었는데 \b 로 하니까 괜찮아졌다.
+print(re.findall(r'\ba\w{3}\b', story))
+
+
+
+# 2번
 # r로 끝나는 모든 단어를 찾는다.
 print(re.findall('\w*r\s', story))
 
+# 해답
+print(re.findall(r'\w+r\b', story))
+
+
+
+# 3번
 # a,b,c,d,e중 아무 문자나 3번 연속으로 들어간 단어를 찾는다.
 # ex) b[eca]me
 print(re.findall('\w*[a-e]{3}\w*', story))
 
+# 해답
+print(re.findall('\w*[abcde]{3}\w*', story))
+
+
+# 4번
+def re_change(m):
+    return '{}{}[{}]'.format(m.group('before').upper(), m.group('center'), m.group('after'))
 # re.sub를 사용해서 ,로 구분된 앞/뒤 단어에 대해 앞단어는 대문자화 시키고, 뒷단어는 대괄호로 감싼다.
 # 이 과정에서, 각각의 앞/뒤에 before, after그룹 이름을 사용한다.
 # re.split(',', ,story)
+p = re.compile(r'(?P<before>\w+)(?P<center>\s*,\s*)(?P<after>\w+)')
+result = re.sub(p, re_change, story)
+
+print(result)
---
+++ Assignment/2017/05/24/basic.py
@@ -0,0 +1,133 @@
+import re
+
+
+class Node(object):
+    """
+    HTML태그 하나를 가지는 클래스
+        내부에 다른 클래스를 가질수도 있음
+        가장 큰 범위는 <html></html>
+    """
+    # 원하는 태그 영역 내의 모든 태그를 가져오는 패턴, pattern_tag_base 변수에 할당
+
+    # <{tag}.*?>
+    # 조건: 전달받은 태그명에 속성이 지정되어 있을 경우(클래스명 또는 아이디명이 지정되어 있는 경우)에도 찾아야 한다.
+    # '<전달받은 태그명' 으로 시작하고
+    # ' . (하나의 문자를) * (앞의 항목을 0회 이상 찾는다/없어도 상관없음)' == 단일 문자를 포함하는 패턴을
+    # ' ? (0회 또는 1회 검색 == 없거나 있거나 1번만 검색)` 한다. > 를 만나기 전까지.
+
+    # \s*([.\w\W]*?)\s*
+    # 조건: 태그영역 내의 모든 문자열(탭, 줄바꿈, 개행문자 포함)가 지정되어야 한다.
+    # '\s*' (white space character_탭/줄바꿈/개행문자, 없어도 상관없음)
+    # [. (단일문자) 또는 \w (문자) 또는 \W (비문자)] 를 * (앞의 항목을 0회 이상 찾는다/없어도 상관없음) == 모든 문자열을 포함하는 패턴(태그 영역내의 모든 부분)
+    # () .group()를 통해 추출할 때, 문자만 추출하기 위해 그룹화
+
+    # </{tag>
+    # 전달받은 태그명의 닫는 태그
+    _pattern_tag_base = r'<{tag}.*?>\s*([.\w\W]*?)\s*</{tag}>'
+
+    _pattern_tag_content = r'<[^!]*?>([.\w\W]*)</.*?>'
+
+    def __init__(self, source):
+        self.source = source
+
+    def __str__(self):
+        return '{}\n{}'.format(
+            super().__str__(),
+            self.source
+        )
+
+    def find_tag(self, tag):
+        """
+        주어진 tag 문자열, 또는 문자열의 리스트 반환
+        :param tag: 검색을 원하는 태그. ex)'div'
+        :param source: 태그를 검색할 전체 문자열
+        :return: 검색 결과가 1개일 경우에는 tag문자열로 만든 Node객체, 2개 이상 일 경우에는 tag문자열로 만든 Node의 리스트
+        """
+        pattern = re.compile(self._pattern_tag_base.format(tag=tag))
+        m_list = re.finditer(pattern, self.source)
+        if m_list:
+            return_list = [Node(m.group()) for m in m_list]
+            return return_list if len(return_list) > 1 else return_list[0]
+        return None
+
+    @property
+    def content(self):
+        """
+        Node인스턴스의 내용을 리턴
+        :return: Node(태그)내부의 내용 문자열을 리턴
+        """
+        pattern = re.compile(self._pattern_tag_content)
+        m = re.search(pattern, self.source.strip())
+        if m:
+            return m.group(1).strip()
+        return None
+
+    @property
+    def class_(self):
+        """
+        해당 Node가 가진 class속성의 value를 리턴 (문자열)
+        :return:
+        """
+        pass
+
+
+with open('example.html') as f:
+    html = Node(f.read())
+
+node_div = html.find_tag('div')
+node_p_list = node_div.find_tag('p')
+
+for node_p in node_p_list:
+    print(node_p.content)
+
+
+# pattern_tag_base = r'<{tag}.*?>\s*([.\w\W]*?)\s*</{tag}>'
+#
+#
+# def find_tag(tag, source):
+#     """
+#     주어진 tag 문자열, 또는 문자열의 리스트 반환
+#     :param tag: 검색을 원하는 태그. ex)'div'
+#     :param source: 태그를 검색할 전체 문자열
+#     :return: 검색 결과가 1개일 경우에는 tag문자열, 2개 이상 일 경우에는 tag문자열의 리스트
+#     """
+#     pattern = re.compile(pattern_tag_base.format(tag=tag))
+#     m_list = re.finditer(pattern, source)
+#     if m_list:
+#         return_list = [m.group() for m in m_list]
+#         return return_list if len(return_list) > 1 else return_list[0]
+#     return None
+#
+#
+# # pattern_tag_content = r'^<.*?>([.\w\W]*?)</.*?>$'
+# pattern_tag_content = r'<.*?>([.\w\W]*)</.*?>'
+#
+#
+# def get_tag_content(tag_string):
+#     """
+#     tag 문자열이 주어졌을 때, 해당 tag의 내용을 리턴
+#     :param tag_string: <tag>내용</tag>형태의 문자열
+#     :return: 위 형태에서 '내용'부분
+#     """
+#     pattern = re.compile(pattern_tag_content)
+#     m = re.search(pattern, tag_string.strip())
+#     if m:
+#         return m.group(1)
+#     return None
+#
+#
+# # html문자열 변수에서 'div'태그의 내용을 찾아 반환하는 함수 실행
+# div = find_tag('div', html)
+# p_list = find_tag('p', div)
+# print(p_list)
+# for p in p_list:
+#     print(get_tag_content(p))
+#
+#
+# # 원리
+# pattern_div = re.compile(r'<div.*?>([.\w\W]*?)</div>')
+# m = re.search(pattern_div, html)
+# div = m.group(1)
+#
+# pattern_p = re.compile(r'<p.*?>([.\w\W]*?)</p>')
+# m_list = re.finditer(pattern_p, div)
