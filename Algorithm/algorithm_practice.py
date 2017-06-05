4147a69b0a762ba5bf86bbfbc841493a7016e89f
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-06-05 23:54:10 +0900

    add 6월5일 알고리즘 풀이, 5주차 필기

--- Algorithm/algorithm_practice.py
+++ Algorithm/algorithm_practice.py
@@ -1,9 +1,11 @@
-def nextSqure(n):
-    sqrt = n ** (1 / 2)
+def sum_digit(number):
+    '''number의 각 자릿수를 더해서 return하세요'''
+    result = 0
+    split_number = map(int, str(number))
+    for i in split_number:
+        result += i
+    return result
 
-    if sqrt % 1 == 0:
-        result = int((sqrt + 1) ** 2)
-        return result
-    return 'no'
 
-print(nextSqure(121))
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(sum_digit(7569)))
